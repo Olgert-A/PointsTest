52c9ac11c19d668bdbfcedf3ebb30edfa059d2c2
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2024-10-11 16:37:19 +0500

    user manager deleted
    User manager is not required in this pet project

--- src/main/java/org/pet/logic/UserManager.java
+++
@@ -1,6 +0,0 @@
-package org.pet.logic;
-
-public interface UserManager {
-    void AddUser(int userId, int points) throws IllegalArgumentException;
-    void DeleteUser(int userId) throws IllegalArgumentException;
-}
